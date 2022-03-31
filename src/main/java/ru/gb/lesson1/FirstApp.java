6f178f2eb5eb404c03805ffc9a7a5a141aa4479e
abowo <dimamorgachev@gmail.com>
2022-03-31 16:37:21 +0300

    1

--- src/main/java/ru/gb/lesson1/FirstApp.java
+++ src/main/java/ru/gb/lesson1/FirstApp.java
@@ -1,50 +1,49 @@
+/**
+ * Java 1. Homework 1.
+ *
+ * @author Dmitry Morgachev
+ * @version 31.03.2022
+ */
 package ru.gb.lesson1;
 
 import jdk.internal.org.objectweb.asm.tree.analysis.Value;
 
-public class FirstApp {
+class FirstApp {
+
     public static void main (String[] args) {
         printThreeWords ();
         checkSumSign ();
         printColor ();
         compareNumbers ();
     }
-    public static void printThreeWords () {
+
+    static void printThreeWords () {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
     }
-    public static void checkSumSign () {
+
+    static void checkSumSign () {
         int a = 5;
         int b = -32;
-        if (a + b >= 0) {
-            System.out.println("Сумма положительна");
-        }
-        else {
-            System.out.println("Сумма отрицательна");
-        }
+        System.out.println(a + b >= 0? "Сумма положительна" : "Сумма отрицательна");
     }
-    public static void printColor () {
+
+    static void printColor () {
         int value = 13;
         if (value <= 0) {
             System.out.println("Красный");
-        }
-        if (value > 0 && value <= 100) {
+        } else if (value <= 100) {
             System.out.println("Желтый");
-        }
-        if (value > 100) {
+        } else {
             System.out.println("Зеленый");
         }
     }
-    public static void compareNumbers () {
+
+    static void compareNumbers () {
         int a = 99;
         int b = 1;
-        if (a >= b) {
-            System.out.println("a >= b");
-        }
-        else {
-            System.out.println("a < b");
-        }
+        System.out.println(a >= b? "a >= b" : "a < b");
     }
 
 }
